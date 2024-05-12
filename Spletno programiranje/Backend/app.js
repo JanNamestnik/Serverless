e33c329b8b1d893a2ca9c893204b15e50f20e8bb
Nejc Cekuta <nejc.cekuta@gmail.com>
2024-05-13 00:29:11 +0200

    SERVERLESS-74
    Povezovanje z mongodb Atlas podatkovno bazo

--- Spletno programiranje/Backend/app.js
+++ Spletno programiranje/Backend/app.js
@@ -4,6 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://admin:hpTmFEirA3PWFYSB@cluster0.jor2wzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRoutes');
 
