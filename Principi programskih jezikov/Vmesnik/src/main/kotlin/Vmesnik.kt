a196cc27f6a5fe12557d9f1968ac914b73c12530
Nejc Cekuta <nejc.cekuta@gmail.com>
2024-05-19 19:31:07 +0200

    SERVERLESS-104
    Oblikovanje menija aplikacije

--- Principi programskih jezikov/Vmesnik/src/main/kotlin/Vmesnik.kt
+++ Principi programskih jezikov/Vmesnik/src/main/kotlin/Vmesnik.kt
@@ -1,28 +1,51 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var selectedScreen by remember { mutableStateOf("Add event") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Row(modifier = Modifier.fillMaxSize()) {
+            Sidebar(selectedScreen) { selectedScreen = it }
         }
     }
 }
+
+@Composable
+fun Sidebar(selectedScreen: String, onScreenSelected: (String) -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .width(200.dp)
+            .padding(12.dp)
+            .border(
+                border = BorderStroke(1.dp, Color.LightGray),
+                shape = RoundedCornerShape(12.dp),
+            )
+            .padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+    }
+}
+
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
