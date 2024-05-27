c061cbe231caf583b668e3387a85a1ecc889c570
meta2003 <metod.golob.2003@gmail.com>
2024-05-27 13:05:34 +0200

    dodana animacija

--- Spletno programiranje/Frontend/src/pages/Home.tsx
+++ Spletno programiranje/Frontend/src/pages/Home.tsx
@@ -2,8 +2,52 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className=" p-20">
-      Pozdravljnei na strani za ogled dogodkov v tvoji bližini
+    <div className=" p-20 text-xl w-full font-bold ">
+      Dobrodošli na naši spletni strani! Smo izjemno veseli in hvaležni, da ste
+      se odločili obiskati našo platformo. Vaša prisotnost nam pomeni več, kot
+      si lahko predstavljate, saj vsak obiskovalec predstavlja dragocen del naše
+      skupnosti. Naša spletna stran je zasnovana z mislijo na vas, uporabnike,
+      in se trudimo, da bi vam ponudili najboljše možne vsebine, storitve in
+      izkušnje. Verjamemo, da ste tukaj, ker cenite kakovostno vsebino,
+      inovativne rešitve in uporabniku prijazno okolje. Naša ekipa razvijalcev
+      neutrudno dela, da bi vam prinesla najbolj sveže in uporabne informacije
+      ter funkcionalnosti, ki vam olajšajo vsakodnevno življenje. Razvijamo se s
+      hitrim tempom in nenehno iščemo nove načine, kako izboljšati vaše
+      uporabniške izkušnje in zadovoljiti vaše potrebe. Vaša podpora je za nas
+      izjemno pomembna. S svojo prisotnostjo, deljenjem vsebin in povratnimi
+      informacijami prispevate k temu, da lahko rastemo in se razvijamo. Vsaka
+      vaša donacija, ne glede na njeno velikost, je za nas neprecenljiva.
+      Donacije nam omogočajo, da nadaljujemo s svojim delom, izboljšujemo naše
+      storitve in še naprej vlagamo v razvoj novih funkcionalnosti. Razvijalci,
+      ki stojijo za to spletno stranjo, so strastni in predani posamezniki, ki
+      neprestano stremijo k inovacijam in izboljšavam. Njihovo delo zahteva
+      veliko časa, znanja in virov, zato je vsaka vaša donacija nepogrešljiv del
+      naše rasti in napredka. Vaš prispevek nam omogoča, da ustvarjamo vsebine,
+      ki vam pomagajo, vas informirajo in zabavajo. Zato vas prijazno vabimo, da
+      razmislite o možnosti donacije. S tem ne boste le podprli naših
+      prizadevanj, ampak boste tudi neposredno prispevali k boljši prihodnosti
+      naše skupnosti. Donacije so enostavne in varne, vsak prispevek pa je
+      neposredno usmerjen v izboljšanje naše platforme in storitev. Prav tako bi
+      se vam želeli posebej zahvaliti za vaš čas in pozornost, ki ste ju
+      namenili branju naše politike o varnosti podatkov. Vaša zasebnost in
+      varnost vaših podatkov sta za nas najvišji prioriteti. Zavedamo se, kako
+      pomembno je, da se počutite varno in zaupljivo pri uporabi naše platforme,
+      zato smo vložili veliko truda v oblikovanje in izvajanje strogih
+      varnostnih ukrepov. Branje in razumevanje naše politike o varnosti
+      podatkov kaže na vašo zavzetost za varno in odgovorno uporabo naše spletne
+      strani. Vaša skrb za varnost podatkov nam omogoča, da še naprej
+      izpolnjujemo naše zaveze glede zaščite vaših osebnih podatkov in
+      zagotavljamo, da so vaše informacije vedno obravnavane z najvišjo stopnjo
+      varnosti in spoštovanja. Iskrena hvala, ker ste del naše zgodbe. Vaša
+      podpora in zaupanje nam veliko pomenita. Skupaj lahko dosežemo izjemne
+      stvari in z vašo pomočjo bomo še naprej rasli in ustvarjali boljšo
+      prihodnost za vse nas. Če imate kakršnakoli vprašanja, predloge ali
+      povratne informacije, vas vabimo, da nas kontaktirate. Z veseljem vam bomo
+      prisluhnili in se potrudili, da izpolnimo vaše želje in potrebe. Še
+      enkrat, hvala za obisk in podporo. Veselimo se nadaljnjega sodelovanja z
+      vami in upamo, da boste uživali v uporabi naše spletne strani. Skupaj
+      lahko dosežemo izjemno!
+      <br /> S spoštovanjem, Razvijalna ekipa podjetja Serverless
     </div>
   );
 };
--- Spletno programiranje/Frontend/src/pages/Map.tsx
+++ Spletno programiranje/Frontend/src/pages/Map.tsx
@@ -1,5 +1,5 @@
 // Map.tsx
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
@@ -7,8 +7,13 @@ import { ArrowLeft } from "@mui/icons-material";
 
 export default function Map() {
   const navigate = useNavigate();
-  const [events, setEvents] = React.useState<MyEvent[]>([]);
+  const [events, setEvents] = useState<MyEvent[]>([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [isAnimating, setIsAnimating] = useState(true);
+  const [animationEnded, setAnimationEnded] = useState(false);
   const token = Cookies.get("token");
+  const markersRef = useRef<L.Marker[]>([]);
+
   useEffect(() => {
     fetch("http://localhost:3000/events", {
       method: "GET",
@@ -21,14 +26,98 @@ export default function Map() {
       .then((data) => {
         setEvents(data);
       });
-  }, []);
+  }, [token]);
+
+  useEffect(() => {
+    let interval: number | null = null;
+
+    if (isAnimating) {
+      interval = window.setInterval(() => {
+        setCurrentDate((prevDate) => {
+          const newDate = new Date(prevDate);
+          newDate.setDate(newDate.getDate() + 1);
+          return newDate;
+        });
+      }, 100); // Change date every second
+    }
+
+    // Check if there are any events happening on the current date
+    const hasEventOnCurrentDate = events.some(
+      (event) =>
+        new Date(event.date_start).toDateString() === currentDate.toDateString()
+    );
+
+    if (hasEventOnCurrentDate) {
+      setTimeout(() => {
+        setIsAnimating(true);
+      }, 5000); // Pause animation for 5 seconds if event on current date
+    }
+
+    return () => {
+      if (interval !== null) clearInterval(interval);
+    };
+  }, [isAnimating, currentDate, events]);
+
+  useEffect(() => {
+    if (events.length > 0) {
+      const maxDate = new Date(
+        Math.max(...events.map((event) => new Date(event.date_start).getTime()))
+      );
+      if (currentDate > maxDate) {
+        setIsAnimating(false);
+        setAnimationEnded(true);
+      } else {
+        setAnimationEnded(false);
+      }
+    }
+  }, [currentDate, events]);
+
+  useEffect(() => {
+    if (isAnimating) {
+      markersRef.current.forEach((marker) => marker.closePopup());
+      const filteredMarkers = markersRef.current.filter((marker) => {
+        const event = marker.options.event as MyEvent;
+        return (
+          new Date(event.date_start).toDateString() ===
+          currentDate.toDateString()
+        );
+      });
+      filteredMarkers.forEach((marker) => marker.openPopup());
+    } else if (animationEnded) {
+      markersRef.current.forEach((marker) => marker.openPopup());
+    }
+  }, [currentDate, isAnimating, animationEnded]);
+
+  const filteredEvents = isAnimating
+    ? events.filter(
+        (event) =>
+          new Date(event.date_start).toDateString() ===
+          currentDate.toDateString()
+      )
+    : events;
 
   function handleDetails(_id: string): void {
     navigate("/event/" + _id);
   }
 
+  function resetAnimation() {
+    if (events.length > 0) {
+      setCurrentDate(new Date(events[0].date_start));
+      setIsAnimating(true);
+    }
+  }
+
   return (
     <div className="pt-20">
+      <button
+        className="mb-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={resetAnimation}
+      >
+        Reset Animation
+      </button>
+      <div className="mb-4 text-lg font-bold">
+        Current Date: {currentDate.toDateString()}
+      </div>
       <MapContainer
         center={[46.55465, 15.645881]}
         zoom={13}
@@ -38,20 +127,26 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {events.map((event, index) => (
+        {filteredEvents.map((event, index) => (
           <Marker
-            key={index}
+            key={event._id}
             position={[
               event.location.coordinates[1],
               event.location.coordinates[0],
             ]}
+            ref={(marker) => {
+              if (marker) {
+                markersRef.current[index] = marker;
+                marker.options.event = event;
+              }
+            }}
           >
             <Popup>
               <div>
                 <h1>{event.name}</h1>
                 <p>{event.description}</p>
                 <button
-                  className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 ps-1 pe-4 rounded-full f"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 ps-1 pe-4 rounded-full"
                   onClick={() => handleDetails(event._id)}
                 >
                   <ArrowLeft />
